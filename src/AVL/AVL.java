b52b41ea5f01a5ea934599fa5c23f2e16bcd3dfc
Jason Peak <jpeak5@lsu.edu>
2011-10-11 21:36:01 -0500

    more comments, handling additional balance factor update cases (1-6)

--- src/AVL/AVL.java
+++ src/AVL/AVL.java
@@ -6,71 +6,131 @@ public class AVL {
 
 	public void insert(Node node, Node target) {
 		boolean newHeight = false;// the global flag that marks when height has
-									// changed
+		// changed
 		if (root == null) {
 			root = node;
 			node.bf = 0;
-		} else if (node.key > target.key) {// is the node to insert bigger than the target? go right
-			if (target.right != null) {//is there already a child on the right of the target?
-				insert(node, target.right);//retry the insertion on target's right child
-			} else {//there is space available for us to insert, perform all actions required to complete insertion
+		} else if (node.key > target.key) {// is the node to insert bigger than
+			// the target? go right
+			if (target.right != null) {// is there already a child on the right
+				// of the target?
+				insert(node, target.right);// retry the insertion on target's
+				// right child
+			} else {// there is space available for us to insert, perform all
+				// actions required to complete insertion
 				target.right = node;
 				node.parent = target;
 				node.bf = 0;// initial BF will be 0 for any new node
-				
+
 				/*
-				 * now we need to determine how or if target  
-				 * (aka the parent of the node just inserted) 
-				 * balance factor should be updated and what should happen as a result
+				 * now we need to determine how or if target (aka the parent of
+				 * the node just inserted) balance factor should be updated and
+				 * what should happen as a result
 				 */
-				if (target.bf == 1) {//left-heavy subtree
-					newHeight = false;//since we are adding to the right, we are correcting the existing imbalance
-					target.bf = 0;//with that done, the balance factor of the root of this subtree is now neutral
+				if (target.bf == 1) { // left-heavy subtree
+					System.out.println("Case 1, bf = 1 => 0");
+					newHeight = false;
+					/*
+					 * since we are adding to the right, we are correcting the
+					 * existing imbalance with that done, the balance factor of
+					 * the root of this subtree will be now neutral
+					 */
 
-				} else if (target.bf == -1) {//target is already heavy on the right side, so we will be increasing the height
-					newHeight = true;//this should demand a left rotation
+				} else if (target.bf == -1) {
+					/*
+					 * target is already heavy on the right side, so by
+					 * inserting on the right, we will be increasing the height
+					 * and we will need to try to fix this state by performing a
+					 * rotation
+					 */
+					System.out.println("Case 2, bf = -1 => -2");
+					newHeight = true; // this should demand a left rotation
 					System.out.println("Left Rotation needed");
+					target.height++;
 				} else if (target.bf == 0) {// single left-rotation
-					System.out.println("previously balanced subtree is now right-heavy");
-					newHeight = true;//the height of this subtree has just increased by 1, which is fine unless it pushes ancestors' heights over 1
-//					target.bf = -1;
+					System.out
+							.println("previously balanced subtree is now right-heavy");
+					System.out.println("Case 3, bf = 0 => -1");
+					newHeight = true;
+					/*
+					 * the height of this subtree has just increased by 1, which
+					 * is fine unless it pushes ancestors' heights over 1
+					 */
 
-					// walk straight up the tree incrementing the heights of the
-					// ancestors
-
-					Node x = target;
-
-					while (x != root && x != null) {
-						x.height++;
-						int xRT = (x.right == null) ? -1 : x.right.height;
-						int xLF = (x.left == null) ? -1 : x.left.height;
-						x.bf = xLF - xRT;
-						if(x.bf>=Math.abs(2)){
-							System.out.println("OMG, rotate");
-						}
-						x = x.parent;
-
-					}// end ancestor walk
-
-					// set the value of the root.height
-					int rRT = (root.right == null) ? -1 : root.right.height; //check null
-					int rLF = (root.left == null) ? -1 : root.left.height; //check null
-					root.bf = rLF - rRT; //set balance factor for root
-					root.height = Math.max(rRT, rLF) + 1;//set height for root
-					// end set root value
 				}
-
+				target.bf--;
 			}
 		} else {
+			/*
+			 * if we didn't insert to the right, then we will try to do so on
+			 * the left
+			 */
 			if (target.left != null) {
 				insert(node, target.left);
 			} else {
-				target.left = node;
-				node.bf = 0;// initial BF will be 0
+				target.left = node; // we have found an appropriate empty spot
+				node.bf = 0; // initial BF will be 0
 				node.parent = target;
+			}
+			/*
+			 * now we need to determine how or if target (aka the parent of
+			 * the node just inserted) balance factor should be updated and
+			 * what should happen as a result
+			 */
+			if (target.bf == 1) { // left-heavy subtree
+				System.out.println("Case 4, bf = 1 => 2");
+				newHeight = true;
+				/*
+				 * since we are adding to the left, we are exacerbating the
+				 * existing imbalance 
+				 */
+
+			} else if (target.bf == -1) {
+				/*
+				 * we are bringing the subtree to equilibrium
+				 * bf will be 0 after insertion
+				 */
+				System.out.println("Case 5, bf = -1 => 0");
+				newHeight = false; 
+				System.out.println("Left Rotation needed");
+				target.height++;
+			} else if (target.bf == 0) {// single left-rotation
+					
+				System.out.println("Case 6, bf = 0 => 1");
+				newHeight = true;
+				/*
+				 * the height of this subtree has just increased by 1, which
+				 * is fine unless it pushes ancestors' heights over 1
+				 */
 
 			}
+			target.bf++;
 		}
+		if (newHeight = true) {
+			/*
+			 * walk straight up the tree incrementing the heights and checking
+			 * balance factors of the ancestors
+			 */
+			Node x = target;
+			while (x != root && x != null) {
+				x.height++;
+				int xRT = (x.right == null) ? -1 : x.right.height;
+				int xLF = (x.left == null) ? -1 : x.left.height;
+				x.bf = xLF - xRT;
+				if (x.bf >= Math.abs(2)) {
+					System.out.println("OMG, rotate");
+				}
+				x = x.parent;
+
+			}// end ancestor walk
+
+			// set the value of the root.height
+			int rRT = (root.right == null) ? -1 : root.right.height; // check
+			// null
+			int rLF = (root.left == null) ? -1 : root.left.height; // check null
+			root.bf = rLF - rRT; // set balance factor for root
+			root.height = Math.max(rRT, rLF) + 1; // set height for root
+		}// end set root value
 	}
 
 	public Node min(Node node) {
@@ -149,15 +209,14 @@ public class AVL {
 		Node node = get(root, key);
 		if (node.left != null) {// go down and right
 			return max(node.left);
-		} else if (node.parent!=null){
-			if(node.parent.right == node) {// look up and left
+		} else if (node.parent != null) {
+			if (node.parent.right == node) {// look up and left
 				return node.parent;
-		}
-			else {
+			} else {
 				return predecessor(node.parent.key);
 			}
-		} 
-		return node=null;
+		}
+		return node = null;
 	}
 
 	//
